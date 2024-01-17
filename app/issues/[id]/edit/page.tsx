6e71406066ba092c01b8c229d2009fdc921dff42
“teekize” <elvisteeka1@gmail.com>
2024-01-17 17:55:21 +0300

    built edit issue page

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import IssueFrom from "../../_components/IssueForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+  return <IssueFrom issue={issue} />;
+};
+
+export default EditIssuePage;
