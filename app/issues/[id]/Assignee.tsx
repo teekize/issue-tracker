70f533d655de6c4790d61b03de4272c531f35582
“teekize” <elvisteeka1@gmail.com>
2024-01-25 09:41:51 +0300

    fetch data with react query

--- app/issues/[id]/Assignee.tsx
+++ app/issues/[id]/Assignee.tsx
@@ -1,26 +1,30 @@
 "use client";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Assignee = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const { data: users, error, isLoading } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUser();
-  }, []);
+  if (error) return null;
+
+  if (isLoading) return <Skeleton />;
+
   return (
     <Select.Root size="3">
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
